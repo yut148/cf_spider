2dd15a083c49e90d94b457fca585227c3f0d6888
khrtz <garula.oze498@gmail.com>
2016-07-21 09:16:54 +0900

    add

--- crawler/campfire_all.rb
+++ crawler/campfire_all.rb
@@ -14,11 +14,11 @@ total_amount = 0
 total_open_project = 0
 
 doc = Nokogiri::HTML.parse(html, nil, charset)
-1.upto(9000) do |page|
+9001.upto(10000) do |page|
   begin
       doc = Nokogiri::HTML(open("https://camp-fire.jp/projects/view/#{page}/"))
     rescue OpenURI::HTTPError
-      sleep 1
+      sleep 2
       next
     end
 
@@ -31,12 +31,12 @@ doc = Nokogiri::HTML.parse(html, nil, charset)
       p node.xpath('//*[@id="js-container"]/div/aside/section[1]/div[2]/a').inner_text
       p "https://camp-fire.jp#{node.xpath('//*[@id="js-container"]/div/aside/section[1]/div[2]/a')[0]['href']}"
 
-      unless node.xpath('section[2]/div[2]/section[1]/div[2]/div[2]/text()').inner_text.gsub(/[^0-9]/,'').to_i == 0
-          p node.xpath('section[2]/div[2]/section[1]/div[2]/div[2]/text()').inner_text.gsub(/[^0-9]/,'').to_i
-          p node.css('section[2]/div[2]/section[1]/div[2]/strong').inner_text.gsub(/[^0-9]/,'').to_i
-        else
+      if node.xpath('section[2]/div[2]/section[1]/div[2]/div[2]/text()').inner_text.gsub(/[^0-9]/,'').to_i == 0
           p node.xpath('section[2]/div[2]/section[2]/p/text()[1]').inner_text.gsub(/[^0-9]/,'').to_i
           p node.xpath('section[2]/div[2]/section[2]/p/text()[2]').inner_text.gsub(/[^0-9]/,'').to_i
+        else
+          p node.xpath('section[2]/div[2]/section[1]/div[2]/div[2]/text()').inner_text.gsub(/[^0-9]/,'').to_i
+          p node.css('section[2]/div[2]/section[1]/div[2]/strong').inner_text.gsub(/[^0-9]/,'').to_i
       end
 
       p node.css('section[1]/div/ul/li/a').inner_text
@@ -54,8 +54,15 @@ doc = Nokogiri::HTML.parse(html, nil, charset)
         file.puts "https://camp-fire.jp/projects/view/#{page}"
         file.puts node.xpath('//*[@id="js-container"]/div/aside/section[1]/div[2]/a').inner_text
         file.puts "https://camp-fire.jp#{node.xpath('//*[@id="js-container"]/div/aside/section[1]/div[2]/a')[0]['href']}"
-        file.puts node.xpath('section[2]/div[2]/section[1]/div[2]/div[2]/text()').inner_text.gsub(/[^0-9]/,'').to_i
-        file.puts node.css('section[2]/div[2]/section[1]/div[2]/strong').inner_text.gsub(/[^0-9]/,'').to_i
+
+        if node.xpath('section[2]/div[2]/section[1]/div[2]/div[2]/text()').inner_text.gsub(/[^0-9]/,'').to_i == 0
+            file.puts node.xpath('section[2]/div[2]/section[2]/p/text()[1]').inner_text.gsub(/[^0-9]/,'').to_i
+            file.puts node.xpath('section[2]/div[2]/section[2]/p/text()[2]').inner_text.gsub(/[^0-9]/,'').to_i
+          else
+            file.puts node.xpath('section[2]/div[2]/section[1]/div[2]/div[2]/text()').inner_text.gsub(/[^0-9]/,'').to_i
+            file.puts node.css('section[2]/div[2]/section[1]/div[2]/strong').inner_text.gsub(/[^0-9]/,'').to_i
+        end
+
         file.puts node.css('section[1]/div/ul/li/a').inner_text
         file.puts node.xpath('section[2]/div[1]/section[2]/div/ul[1]/li[2]/a').inner_text.delete('#')
         file.puts node.css('section[2]/div[2]/section[1]/div[3]/strong').inner_text
@@ -63,7 +70,6 @@ doc = Nokogiri::HTML.parse(html, nil, charset)
         file.puts node.css('section[2]/div[2]/section[2]/p/strong[1]').inner_text.delete('に')
         file.puts node.css('section[2]/div[2]/section[2]/p/strong[4]').inner_text
         file.puts node.css('section[2]/div[2]/section[2]/p/strong[3]').inner_text
-        file.puts ""
       end
 
 
